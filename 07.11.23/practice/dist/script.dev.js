5c723ef9e7cc64bdd318d6c4d683e7d0fad45b45
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-11-09 20:49:59 +0100

    modified:   07.11.23/practice/dist/script.dev.js 	modified:   07.11.23/practice/script.js

--- 07.11.23/practice/dist/script.dev.js
+++ 07.11.23/practice/dist/script.dev.js
@@ -1,1 +1,11 @@
 "use strict";
+
+var student = {
+  name: "Gordon",
+  surname: "Freeman",
+  age: 18,
+  batch: "FS-Jun21",
+  hasWebcam: true
+};
+var somma = Math.add(5, 3);
+console.log(somma);
---
+++ 07.11.23/practice/script.js
@@ -0,0 +1,11 @@
+let student = {
+    name: "Gordon",
+    surname: "Freeman",
+    age: 18,
+    batch: "FS-Jun21",
+    hasWebcam: true,
+};
+
+let somma = Math.add(5, 3);
+
+console.log(somma);
