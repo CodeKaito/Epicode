2ade2236660c713647b329ea009389691ee2916a
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-11-09 18:47:27 +0100

    new file:   06.11.23/practice/dist/script.dev.js 	new file:   07.11.23/index.html 	new file:   07.11.23/practice/script.js 	new file:   07.11.23/theory/README.md 	new file:   template/index.html

---
+++ 06.11.23/practice/dist/script.dev.js
@@ -0,0 +1,32 @@
+// TODO introduzione a javascript
+// Alcuni tipi di dati fondamentali
+// 1. String es. "Nome"
+// 2. Number es. 1
+// 3. Float es. 1.3
+// 4. Boolean es. true or false
+// 5. Undefined es. valore della variabile di default
+// 6. Null es. assenza di valore
+// Cos'é un console.log()
+// Serve a stampare il contenuto dell'argomento
+//? Es.1 Stampa la stringa Hello world! in console.
+//console.log("Hello world!");
+//? Es.2 Commenti in javascript (single-line multi-line comments)
+// Questo é un commento single-line comment
+
+/*
+    Questo é un commento multi-line comment
+*/
+//? Es.3 Scegliere un nome per una variabile (convenzioni e camelCase)
+// var myVariable = 8;
+//? Es.4 
+//? Es.5 Se volessi ora annullare il valore della variabile?
+// let mySecondVar;
+// console.log(mySecondVar);
+//? Es.7 Prepariamo i biscotti
+// let butter = 120;
+// let sugar = 80;
+// let flour= 260;
+// let mediumBowl;
+// mediumBowl+=sugar;
+// console.log(mediumBowl);
+"use strict";
