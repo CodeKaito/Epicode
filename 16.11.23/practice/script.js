becfe25b6a635f38e7a7d5a775bb885b135e147c
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-11-16 20:00:33 +0100

    new file:   16.11.23/practice/script.js

---
+++ 16.11.23/practice/script.js
@@ -0,0 +1,27 @@
+// let arr = [1, "2", { three: 3}, [4, 5, [8, 9 ]] ];
+
+// console.log(arr[3][2][0]);
+
+// let students = [
+//     { name: "Paulina", note: 5 },
+//     { name: "Alex", note: 6 },
+//     { name: "Diana", note: 4 },
+// ];
+
+// console.log(students[1].name + "'s note is " + students[1].note);
+
+// let matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+// ];
+
+// console.log(String(matrix[0][1]) + matrix[1][2] + matrix[2][0]);
+
+// let array1 = [1, 2, 3];
+// let array2 = [4, 5, 6];
+
+// let newArray = array1.concat(array2);
+
+// console.log(newArray);
+
