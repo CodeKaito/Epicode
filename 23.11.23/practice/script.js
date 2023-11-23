94f84e31cc25c5984561dffce65c6ba77d817e07
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-11-23 21:43:12 +0100

    modified:   20.11.23/practice/script.js 	new file:   23.11.23/index.html 	new file:   23.11.23/practice/index.html 	new file:   23.11.23/practice/script.js new file:   23.11.23/task/README.md

---
+++ 23.11.23/practice/script.js
@@ -0,0 +1,11 @@
+// Crea un oggetto Persona con una age ed un metodo per salutare
+
+const person = {
+    age: 28,
+    greet: () => {
+        console.log("Hello to all");
+    }
+}
+
+console.log(person.age);
+person.greet();
