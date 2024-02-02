e20011aa38601cb5420df83db433c93dcb5ffdcc
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2024-02-02 11:52:06 +0100

    update 	modified:   24.02.01/practice/fetch.js

--- 24.02.01/practice/fetch.js
+++ 24.02.01/practice/fetch.js
@@ -1,7 +1,7 @@
 // https://triveschool-api.herokuapp.com/api/agenda
 // https://triveschool-api.herokuapp.com/api/deezer/search?q=metallica
 
-fetch("https://triveschool-api.herokuapp.com/api/agenda")
+fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica")
     .then((response) => response.json())
     .then((json) => console.log(json))
     .catch((err) => console.error("Error detected: ", err));
