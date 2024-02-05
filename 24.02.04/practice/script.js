09d3904845f58701a7bd6809b6d10c3037ffe4f3
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2024-02-05 21:50:18 +0100

    update

---
+++ 24.02.04/practice/script.js
@@ -0,0 +1,31 @@
+//! Array methods
+
+//? 1. ilter()
+//* Input: callback, Output nuovo array modificato secondo callback function.
+//TODO: salvare in un nuovo array solo gli elementi < 5 di myArray
+// let myArray = [ 1, 3, 5, 9, 2 ];
+
+// let lowerThanFive = myArray.filter((element) => {
+//     return element < 5;
+// });
+
+//? 2. foreach
+//* Input: callback del tipo (element, index) => { ... }, Output; nessuno.
+//TODO: per ogni elemento di myArray, eseguire un console.log del tipo: indice loop - elemento x2.
+// let myArray = [ 1, 3, 5, 9, 2 ];
+
+// myArray.forEach((element, i) => {
+//     console.log( "Io sono l'indice " + i + ", L'elemento x2 é: " + element*2 );
+// })
+
+//TODO: Per ogni elemento di myStringArray, eseguire un console.log dei vari elementi in Uppercase
+// let myArray = [ "Abaco", "Ciao", "Bello", "Italia", "Salve" ];
+
+// myArray.forEach((element) => {
+//     console.log(element.toUpperCase());
+// });
+
+//? 3. map
+//* Input: 
+// let myArray = [ 1, 3, 5, 9, 2 ];
+
